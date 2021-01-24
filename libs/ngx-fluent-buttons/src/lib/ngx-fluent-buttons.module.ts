005a360e042d4819e2fbe15c42271c12d1f254d5
Andres Mojica <andres.mojica@outlook.de>
2021-01-24 12:14:11 +0100

    lib working

--- libs/ngx-fluent-buttons/src/lib/ngx-fluent-buttons.module.ts
+++ libs/ngx-fluent-buttons/src/lib/ngx-fluent-buttons.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+
 import { SplitButton } from './splitbutton/splitbutton';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRippleModule,
+  ],
   declarations: [SplitButton],
   exports: [SplitButton],
 })
